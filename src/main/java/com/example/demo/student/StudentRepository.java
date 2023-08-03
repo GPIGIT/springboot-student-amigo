feb0b4a1f56cd050338c4202f930499eb566a3fb
GPIGIT <georgi.p.ivanovbg@gmail.com>
2023-08-03 22:14:47 +0300

    created data access layer - create StudentRepository interface and create Http GET method

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository                                 //      <typeForTable,PK type>
+@Repository                                 //      <typeOfObjectforTable,PK type>
 public interface StudentRepository extends JpaRepository<Student,Long> {
 }
