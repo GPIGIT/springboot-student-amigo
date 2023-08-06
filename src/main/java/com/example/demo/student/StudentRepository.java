f62f211da246756f20d995a38fce96e1f9b26216
GPIGIT <georgi.p.ivanovbg@gmail.com>
2023-08-06 19:02:47 +0300

    create POST Mapping Request

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,8 +1,17 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository                                 //      <typeOfObjectforTable,PK type>
 public interface StudentRepository extends JpaRepository<Student,Long> {
+
+    //Here WRITE  CUSTOM FUNCTIONS
+
+    // This HIBENRATE will convert to Query: SELECT & FROM student WHERE EMAIL = ? (What we pass here will take for param)
+//    @Query("SELECT s FROM Student s WHERE s.email = ?1") //this is JPQL
+    Optional<Student> findStudentByEmail(String email);
 }
